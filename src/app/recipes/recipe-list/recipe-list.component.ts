ec672af5440a57a10282b16d37d43547ef2c03af
Jiani Liu <liujiani505@gmail.com>
2023-01-18 01:15:40 -0500

    fixed styling

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,6 +10,7 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] =[
     new Recipe('A test recipe', 'just a test', 'https://picturetherecipe.com/wp-content/uploads/2020/07/Butter-Chicken-PTR-Featured-395x500.jpg'),
+    new Recipe('A test recipe', 'just a test', 'https://picturetherecipe.com/wp-content/uploads/2020/07/Butter-Chicken-PTR-Featured-395x500.jpg'),
     new Recipe('A test recipe', 'just a test', 'https://picturetherecipe.com/wp-content/uploads/2020/07/Butter-Chicken-PTR-Featured-395x500.jpg')
   ]
 
