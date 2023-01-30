8721b73f907b8fca9dcc12fe5f16b9f5806fc763
Jiani Liu <liujiani505@gmail.com>
2023-01-30 16:37:46 -0500

    added navigation using event binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HomeChef';
+
+  displayedFeature = 'recipe';
+  onNavigate(feature: string){
+    this.displayedFeature = feature;
+  }
 }
