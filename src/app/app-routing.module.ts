207f08e317ce6704615aa5ae4af9e8b5b0266cfb
Jiani Liu <liujiani505@gmail.com>
2023-06-07 13:48:12 -0400

    adding child routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 
 const approutes: Routes = [
   {path: '', redirectTo:'/recipes', pathMatch:'full'},
-  {path: 'recipes', component: RecipesComponent},
+  {path: 'recipes', component: RecipesComponent, children:[
+    { path: ':id', component: RecipeDetailComponent}
+  ]},
   {path: 'shopping-list', component: ShoppingListComponent},
 ];
 
