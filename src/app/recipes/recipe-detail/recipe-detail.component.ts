7990c1cced644b1b3dd90397d60910a7a5984098
Jiani Liu <liujiani505@gmail.com>
2023-06-07 14:07:16 -0400

    fixed a bug

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -18,8 +18,8 @@ export class RecipeDetailComponent implements OnInit {
     this.route.params
     .subscribe(
       (params: Params) =>{
-        this.id = params['id'];
-        this.recipeService.getRecipe(this.id);
+        this.id = +params['id'];
+        this.recipe = this.recipeService.getRecipe(this.id);
       }
     )
   }
