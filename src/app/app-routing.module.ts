b4821e4b8783cf39de8644fcd4a136683caef911
jianiliu <liujiani505@gmail.com>
2023-06-09 11:45:59 -0400

    added edit routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
@@ -8,7 +9,10 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 const approutes: Routes = [
   {path: '', redirectTo:'/recipes', pathMatch:'full'},
   {path: 'recipes', component: RecipesComponent, children:[
-    { path: ':id', component: RecipeDetailComponent}
+    // the new route has to come before the id route, if not when we type /recipes/new, it'll assume new is id, because angular doesn't knew if new is as a new hard coded route or as a dynamic parameter.
+    { path: 'new', component: RecipeEditComponent},
+    { path: ':id', component: RecipeDetailComponent},
+    { path: ':id/edit', component: RecipeEditComponent}
   ]},
   {path: 'shopping-list', component: ShoppingListComponent},
 ];
