9e3d332f6e8fd7b12dea6db43b5aab611c0259b7
jianiliu <liujiani505@gmail.com>
2023-06-09 13:58:29 -0400

    did a cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HomeChef';
-
-  displayedFeature = 'recipe';
-  onNavigate(feature: string){
-    this.displayedFeature = feature;
-  }
 }
+
