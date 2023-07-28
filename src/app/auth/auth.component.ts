85d9b896bc10754d1cd7fde0e50e4e97b9184f82
jianiliu <liujiani505@gmail.com>
2023-07-28 12:43:51 -0400

    added html and styling for auth page

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-auth',
-    templateUrl: './auth.component.html'
+    templateUrl: './auth.component.html',
+    styleUrls: ['./auth.component.css']
 })
 export class AuthComponent{}
