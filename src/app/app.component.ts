9a6ae29a80a261ad1b82574be7dce86e49c3751a
Jiani Liu <liujiani505@gmail.com>
2023-08-08 17:18:47 -0400

    adding auto login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth-layout/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'HomeChef';
+
+  constructor(private authService: AuthService){}
+  ngOnInit(): void {
+    this.authService.autoLogin();
+  }
 }
 
